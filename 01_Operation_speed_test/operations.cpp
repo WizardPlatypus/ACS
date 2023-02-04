24415b760c61d14a69857e7b186f8afd220295d6
WizardPlatypus <f.volik.m@gmail.com>
2023-02-04 09:43:58 +0200

    Renamed and removed duplicate operations

--- 01_Operation_speed_test/operations.cpp
+++ 01_Operation_speed_test/operations.cpp
@@ -5,12 +5,12 @@ T nothing(const T& a, const T& b) {
 }
 
 template<typename T>
-T plus(const T& a, const T& b) {
+T add(const T& a, const T& b) {
     return a + b;
 }
 
 template<typename T>
-T minus(const T& a, const T& b) {
+T subtract(const T& a, const T& b) {
     return a - b;
 }
 
@@ -29,7 +29,3 @@ T modulo(const T& a, const T& b) {
     return a % b;
 }
 
-template<typename T>
-T subtract(const T& a, const T& b) {
-    return a - b;
-}
