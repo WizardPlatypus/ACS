ff5b5ba5096522d756d0c218403d131bb86570de
WizardPlatypus <f.volik.m@gmail.com>
2023-02-04 10:59:01 +0200

    Added a wrapper function responsible for per test i/o.

--- 01_Operation_speed_test/main.cpp
+++ 01_Operation_speed_test/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream> // input/output
 #include <chrono> // timers
 #include <functional> // std::function<>
+#include <string>
+#include <forward_list> // single-linked list datastructure
+
 #include "operations.cpp" // my operation templates
 
+using time_unit = std::chrono::milliseconds;
+
+
 template<typename T>
-std::chrono::milliseconds test_operation(const std::function<T(const T, const T)>& operation, const T begin, const T end, const T step = 1) {
+time_unit test_operation(const std::function<T(const T, const T)>& operation, const T begin, const T end, const T step = 1) {
     size_t execution_count = 0;
     std::chrono::high_resolution_clock::time_point start, finish;
     T result;
@@ -18,7 +24,27 @@ std::chrono::milliseconds test_operation(const std::function<T(const T, const T)
     }
     finish = std::chrono::high_resolution_clock::now();
 
-    return std::chrono::duration_cast<std::chrono::milliseconds>(finish - start);
+    return std::chrono::duration_cast<time_unit>(finish - start);
+}
+
+template<typename T>
+void operation_test_wrapper(const std::string type_label, const std::string operation_label, const int count, const std::function<T(const T, const T)>& operation, const T begin, const T end, const T step = 1) {
+    auto time = test_operation<T>(operation, begin, end, step).count();
+
+    // type, operation, count, time
+    std::forward_list<std::string> data {
+        type_label,
+        operation_label,
+        std::to_string(count),
+        std::to_string(time),
+    };
+
+    std::cout << data.front();
+    data.pop_front();
+    for (auto item: data) {
+        std::cout << "," << item;
+    }
+    std::cout << std::endl;
 }
 
 int main() {
@@ -26,14 +52,7 @@ int main() {
     size_t count = (end - begin + 1) / step;
     count *= count;
 
-    auto add_time = test_operation<int>(add<int>, begin, end, step).count();
-
-    auto nothing_time = test_operation<int>(nothing<int>, begin, end, step).count();
-
-    std::cout << "Count: " << count << std::endl;
-    std::cout << "Add: " << add_time << " ms" << std::endl;
-    std::cout << "Nothing: " << nothing_time << " ms" << std::endl;
-    std::cout << "Time difference is " << add_time - nothing_time << " ms" << std::endl;
+    operation_test_wrapper<int>("int", "add", count, add<int>, begin, end, step);
 
     return 0;
 }
