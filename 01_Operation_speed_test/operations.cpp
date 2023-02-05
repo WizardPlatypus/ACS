5a6aecb9b3b3465f2239d2a27e546a5ef790bc80
WizardPlatypus <f.volik.m@gmail.com>
2023-02-05 09:19:13 +0200

    Added explecit template to combat absense of modulo implementation
    So long as I don't forget that modulo for double and float types is
    now a dummy, this is an easy fix.

--- 01_Operation_speed_test/operations.cpp
+++ 01_Operation_speed_test/operations.cpp
@@ -28,3 +28,11 @@ T modulo(const T& a, const T& b) {
     return a % b;
 }
 
+// there is no standard implementation for operation % on operands of type const double, const float.
+template<> double modulo(const double& a, const double& b) {
+    return 0;
+}
+template<> float modulo(const float& a, const float& b) {
+    return 0;
+}
+
