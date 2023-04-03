c2f11cd51682fb0385a13e315c81ee3fb1bf2295
WizardPlatypus <f.volik.m@gmail.com>
2023-04-03 09:04:57 +0300

    Adressing the need for isolated compilation

--- 03_TCP-IP/build.sh
+++ 03_TCP-IP/build.sh
@@ -1,2 +1,1 @@
-g++ server.cpp shared.cpp -o server.exe -lWs2_32
-g++ client.cpp shared.cpp -o client.exe -lWs2_32 -lMswsock -lAdvApi32
+g++ $1.cpp -o $1.exe -lWs2_32 -lMswsock -lAdvApi32 -std=c++17 -g
